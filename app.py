86e41f46c172cef27924c869c16d886d811d82bb
jeyaakash <spjeyaakash987@gmail.com>
2026-04-01 14:16:50 +0530

    fixed port issue

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,4 +7,6 @@ app = Flask(__name__)
 def home():
     return "DevOps Project Running 🚀"
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
